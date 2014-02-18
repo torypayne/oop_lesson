379c9efbec5650bef81b4e3cea9ce584c31d287e
Hackbright Student <info@hackbrightacademy.com>
2014-02-18 12:56:48 -0800

    Working game with gems and rocks and fun

--- game.py
+++ game.py
@@ -11,13 +11,157 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    IMAGE = "Rock"
+    SOLID = True
+
+class Character(GameElement):
+    IMAGE = "Girl"
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+        return None
+
+class Gem(GameElement):
+    IMAGE = "BlueGem"
+    SOLID = False
+    def interact(self,PLAYER):
+        PLAYER.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!"%(len(PLAYER.inventory)))
+
+
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    GAME_BOARD.draw_msg("This game is awesome.")
+    
+    rock_positions = [
+        (2,1),
+        (1,2),
+        (3,2),
+        (2,3)
+    ]
+
+    rocks = []
+
+    for pos in rock_positions:
+        rock = Rock()
+        print 'rock is instantiated ', rock
+        GAME_BOARD.register(rock)
+        print 'game board is instantiated',GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+
+    rocks[-1].SOLID = False
+
+    # for rock in rocks:
+    #     print rock
+
+    gem = Gem()
+    GAME_BOARD.register(gem)
+    GAME_BOARD.set_el(3, 1, gem)
+
+    global PLAYER
+    PLAYER = Character()
+    GAME_BOARD.register(PLAYER)
+    GAME_BOARD.set_el(2,2, PLAYER)
+    print PLAYER
+
+
+
+
+
+
+
+def keyboard_handler():
+    direction = None
+
+    if KEYBOARD[key.UP]:
+        direction = "up"
+    if KEYBOARD[key.DOWN]:
+        direction = "down"
+    if KEYBOARD[key.LEFT]:
+        direction = "left"
+    if KEYBOARD[key.RIGHT]:
+        direction = "right"
+
+    if direction:
+        next_location = PLAYER.next_pos(direction)
+        next_x = next_location[0]
+        next_y = next_location[1]
+
+        existing_el = GAME_BOARD.get_el(next_x, next_y)
+        if existing_el is None or not existing_el.SOLID:
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
+        if existing_el:
+            existing_el.interact(PLAYER)
+
+
+
+    # if KEYBOARD[key.UP]:
+    #     GAME_BOARD.draw_msg("You pressed up")
+    #     next_y = PLAYER.y - 1
+    #     GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+    #     GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+    # if KEYBOARD[key.DOWN]:
+    #     GAME_BOARD.draw_msg("You pressed down.")
+    #     next_y = PLAYER.y + 1
+    #     GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+    #     GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+    # if KEYBOARD[key.RIGHT]:
+    #     GAME_BOARD.draw_msg("You pressed right.")
+    #     next_x = PLAYER.x + 1
+    #     GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+    #     GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)
+    # if KEYBOARD[key.LEFT]:
+    #     GAME_BOARD.draw_msg("You pressed left.")
+    #     next_x = PLAYER.x - 1
+    #     GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+    #     GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)
+    # elif KEYBOARD[key.SPACE]:
+    #     GAME_BOARD.erase_msg()
+
+
+
+    # rock1 = Rock()
+    # GAME_BOARD.register(rock1)
+    # GAME_BOARD.set_el(2,1,rock1)
+
+    # rock2 = Rock()
+    # GAME_BOARD.register(rock2)
+    # GAME_BOARD.set_el(1,2, rock2)
+
+    # rock3 = Rock()
+    # GAME_BOARD.register(rock2)
+    # GAME_BOARD.set_el(3,2, rock2)
+
+    # rock4 = Rock()
+    # GAME_BOARD.register(rock2)
+    # GAME_BOARD.set_el(2,3, rock2)
+
+    # print "The first rock is at", (rock1.x, rock1.y)
+    # print "The second rock is at", (rock2.x, rock2.y)
+    # print "The third rock is at", (rock3.x, rock3.y)
+    # print "The fourth rock is at", (rock4.x, rock4.y)
+
+    # print "Rock 1 image", rock1.IMAGE
+    # print "Rock 2 image", rock2.IMAGE
+
+
+
